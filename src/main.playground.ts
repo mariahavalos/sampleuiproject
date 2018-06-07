8b8c1b8edd1d1dcc3b85418a87a850daa1b194a5
Mariah <davism90@msu.edu>
2018-06-06 22:22:11 -0400

    fixing

--- src/main.playground.ts
+++ src/main.playground.ts
@@ -1,11 +1,4 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { PlaygroundModule } from 'angular-playground';
+import { AppModule } from './app/app.module';
 
-PlaygroundModule
-  .configure({
-      selector: 'app-root',
-      overlay: false,
-      modules: []
-  });
-
-platformBrowserDynamic().bootstrapModule(PlaygroundModule);
+platformBrowserDynamic().bootstrapModule(AppModule);
